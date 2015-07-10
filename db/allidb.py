f45b996a865afdd9fe55e24500a97d8240574484
DangKhoaSDC <letan.dangkhoa@gmail.com>
2015-07-10 17:02:35 -0600

    function: visualize data

--- db/allidb.py
+++ db/allidb.py
@@ -12,11 +12,16 @@ def visualize_loc(img, points):
     """
     Visualize the detected ALL cells based on their location
     """
-    assert isinstance(img,basestring)
+    assert isinstance(img, basestring)
     assert points is not []
-    im = cv2.imread(img,1)
+    ratio = 0.25
+    im = cv2.imread(img, 1)
+    w, h = int(ratio * im.shape[1]), int(ratio * im.shape[0])
+    im = cv2.resize(im, (w, h))
+    points = [(int(ratio * x), int(ratio * y)) for x, y in points]
     for p in points:
-        cv2.circle(im, p, 20, (1,1,0))
+        cv2.circle(im, p, 5, (255, 0, 0), -1)
+    cv2.namedWindow("Display", cv2.WINDOW_AUTOSIZE)
     cv2.imshow("Display", im)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -28,6 +33,6 @@ if __name__ == '__main__':
     print "Usage:"
     print "python allidb.py image filelist"
     f = open(sys.argv[2], "r")
-    points = [map(int, loc.split()) for loc in f.readlines()]
+    points = [tuple(map(int, loc.split())) for loc in f.readlines()]
 
-    visualize_loc(img, points)
+    visualize_loc(sys.argv[1], points)
