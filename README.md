f02cf410aec2e3a8f22a153b097c221bc07c3ab6
DangKhoaSDC <letan.dangkhoa@gmail.com>
2015-07-13 17:57:48 -0600

    add the Framework class

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ This is my project during Mitacs internship at the university of Saskatchewan.
 # Introduction
 
 This program automated detect cells in ALL-IDB dataset [Link here](http://crema.di.unimi.it/~fscotti/all/).
+
+# Proposed Method
+[Updating]
---
+++ framework.py
@@ -0,0 +1,12 @@
+"""
+File: framework.py
+Author: Khoa Le Tan Dang <letan.dangkhoa@gmail.com>
+Email: letan.dangkhoa@gmail.com
+Github: dangkhoasdc
+Description: The proposed framework
+"""
+
+class Framework(object):
+    """Main Framework"""
+    def __init__(self):
+        """init"""
