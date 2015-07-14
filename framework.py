17d805a0c3b4cc1cb32d9905cf895cac093bc881
DangKhoaSDC <letan.dangkhoa@gmail.com>
2015-07-13 18:07:44 -0600

    Design the Framework class

--- framework.py
+++ framework.py
@@ -8,5 +8,18 @@ Description: The proposed framework
 
 class Framework(object):
     """Main Framework"""
-    def __init__(self):
+    def __init__(self, training_files=None, testing_files=None):
         """init"""
+        self.training_data = training_files
+        self.testing_data = testing_files
+        self.preprocess_method = None
+        self.extract_method = None
+
+    def preprocess(self, method):
+        self.preprocess_method = method
+
+    def extract_features(self, method):
+        self.extract_method = method
+    def train(self):
+        print "training phase"
+
