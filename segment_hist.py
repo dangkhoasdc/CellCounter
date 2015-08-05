650a8b92dc1a295eb31be5ff730a710e902845e9
DangKhoaSDC <letan.dangkhoa@gmail.com>
2015-08-04 18:27:04 -0600

    the best result 88.91

--- segment_hist.py
+++ segment_hist.py
@@ -18,7 +18,7 @@ class SegmentStage(Stage):
     """ Segmentation algorithm """
     def __init__(self, wd_sz=None):
         params = {"wd_sz": wd_sz}
-        self._default_params = {"wd_sz": 20, "dist_tol": 5}
+        self._default_params = {"wd_sz": 20, "dist_tol": 6}
         super(SegmentStage, self).__init__("findContours algorithm", params)
 
     def inside(self, l, s):
@@ -64,20 +64,27 @@ class SegmentStage(Stage):
                     filtered_contours.append(c)
             else:
                 filtered_contours.append(c)
-        contours = list(set(filtered_contours))
+        # contours = list(set(filtered_contours))
+        contours = list(filtered_contours)
+
+        # for c in contours:
+            # c.draw(orig_image, (0, 0, 255), 1)
+        # com.debug_im(orig_image)
+
         contours = [c for c in contours if (c.width >= wd_sz and c.height >= wd_sz) and (1.7 > (c.width/float(c.height)) >= 0.5)]
         contours = [c for c in contours if c.area >= 140]
         contours = [c for c in contours if 8 < c.center[0] and 8 < c.center[1] and c.center[0] < w-8 and c.center[1] < h-8]
         contours = [c for c in contours if 2 < c.lt[0] and 2 < c.lt[1] and c.rb[0] < w-2 and c.rb[1] < h-2]
 
-        for con in contours:
-            for c in contours:
-                if con != c and com.euclid(c.center, con.center) < dist_tol:
-                    contours.remove(c)
         contours = sorted(contours, key= lambda x: x.area, reverse=True)
         filtered_contours = []
         for con in contours:
             result = any([self.inside(con, s) for s in contours if s != con])
             if not result:
                 filtered_contours.append(con)
-        return filtered_contours
+        contours = list(filtered_contours)
+        for con in contours:
+            for c in contours:
+                if con != c and com.euclid(c.center, con.center) < dist_tol:
+                    contours.remove(c)
+        return contours
