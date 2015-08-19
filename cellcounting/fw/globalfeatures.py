8ee18eac020a606b18e18deb9099a041fbc3af87
DangKhoaSDC <letan.dangkhoa@gmail.com>
2015-08-18 22:45:15 -0600

    the framework for the global feature

---
+++ cellcounting/fw/globalfeatures.py
@@ -0,0 +1,7 @@
+"""
+File: globalfeatures.py
+Author: Khoa Le Tan Dang <letan.dangkhoa@gmail.com>
+Email: letan.dangkhoa@gmail.com
+Github: dangkhoasdc
+Description: The framework for the global feature
+"""
